9a9be4cdec8554fbda5fe329f2a0478ce70127f8
Carlos Guimaraes <sekmet@open4g.com>
2018-07-08 12:08:34 -0300

    Update morris.js

--- morris.js
+++ morris.js
@@ -83,7 +83,9 @@ Licensed under the BSD-2-Clause License.
         this.el = $(options.element);
       }
       if ((this.el == null) || this.el.length === 0) {
-        throw new Error("Graph container element not found");
+        //TODO - find a workaround
+        return;
+        //throw new Error("Graph container element not found");
       }
       if (this.el.css('position') === 'static') {
         this.el.css('position', 'relative');
